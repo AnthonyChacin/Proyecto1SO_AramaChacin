c1f82e4cc1032abadf30b016997b47a06174a534
AnthonyChacin <anthony.chacin@correo.unimet.edu.ve>
2018-10-14 16:13:48 -0400

    Creando las clases de productor con sus respectivas extensiones, ensamblador y la pantalla principal de la interfaz

---
+++ src/logica/Productor.java
@@ -0,0 +1,10 @@
+package logica;
+
+/**
+ *
+ * @authores: Anthony Chacin, carné: 20171110998
+ *            Elías Arama, carné: 
+ */
+public abstract class Productor {
+    
+}
---
+++ src/logica/ProductorBateria.java
@@ -0,0 +1,11 @@
+
+package logica;
+
+/**
+ *
+ * @authores: Anthony Chacin, carné: 20171110998
+ *            Elías Arama, carné: 
+ */
+public class ProductorBateria extends Productor {
+    
+}
