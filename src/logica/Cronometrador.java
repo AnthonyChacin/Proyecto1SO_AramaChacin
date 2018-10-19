d1682c3040841dc5079b8fe1eb8455237eac671c
AnthonyChacin <anthony.chacin@correo.unimet.edu.ve>
2018-10-19 17:59:16 -0400

    Guardando cambios por falta de luz

--- src/logica/Cronometrador.java
+++ src/logica/Cronometrador.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package logica;
 
 /**
@@ -10,6 +6,26 @@ package logica;
  * @autores: Anthony Chacin, carné: 20171110998
  *            Elías Arama, carné: 20171110178
  */
-public class Cronometrador {
+public class Cronometrador extends Thread {
     
+    private int contador;
+    private double horasEscribiendo;
+    private double horasDurmiendo;
+    private int diaEnSegundos;
+    
+    public Cronometrador(int contador, int diaEnSegundos){
+        this.contador = contador;
+        this.diaEnSegundos = diaEnSegundos;
+        this.horasEscribiendo = ((this.diaEnSegundos*1000)/24)*1.5;
+        this.horasDurmiendo = (this.diaEnSegundos*1000) - this.horasEscribiendo;
+    }
+    
+    public void configurarHoras(){
+        
+    }
+    
+    @Override
+    public void run(){
+        
+    } 
 }
